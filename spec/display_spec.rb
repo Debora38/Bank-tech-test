88288bd7fdb6f5fe002aa2d6e8935d0046e1d1ed
debora <debora.piu92@gmail.com>
2019-03-12 11:21:24 +0000

    renamed tests to be more clear

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -2,13 +2,13 @@ require 'display'
 
 RSpec.describe Display do
   context "#print_statement" do
-    it 'should print out the history of my account: no deposits' do
+    it 'should print out the history of my account: one deposit' do
       subject.transactions.unshift({date: '04/02/2019', credit: subject.twodecimal(2000), debit: "", balance: subject.twodecimal(2000)})
       expect(subject.print_statement).to include('date || credit || debit || balance')
       expect(subject.print_statement).to include('04/02/2019 || 2000.00 ||  || 2000.00')
     end
 
-    it 'should print out the history of my account: multiple actions' do
+    it 'should print out the history of my account: multiple transactions' do
       allow(Date).to receive_message_chain(:today, :strftime).and_return '04/02/2019'
       subject.save_deposit(2000, 2000)
       subject.save_withdrawal(500, 1500)
