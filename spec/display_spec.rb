dd92f0810a659af341b56df969fef65503fbce0a
debora <debora.piu92@gmail.com>
2019-03-12 10:50:06 +0000

    irrelevant test removed

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -1,12 +1,6 @@
 require 'display'
 
 RSpec.describe Display do
-  context "#transactions" do
-    it "should have an array of transactions" do
-      expect(subject.transactions).to be_kind_of(Array)
-    end
-  end
-
   context "#print_statement" do
     it 'should print out the history of my account: no deposits' do
       subject.transactions.unshift({date: '04/02/2019', credit: subject.twodecimal(2000), debit: "", balance: subject.twodecimal(2000)})
