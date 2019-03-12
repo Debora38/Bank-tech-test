9320ab30bb7197a179ef02aad0881c2d50de18b8
debora <debora.piu92@gmail.com>
2019-03-12 15:41:52 +0000

    twodecimal functionality moved to the account history

--- lib/account_history.rb
+++ lib/account_history.rb
@@ -8,14 +8,19 @@ class AccountHistory
   end
 
   def save_deposit(amount, balance)
-    @all_transactions.unshift(date: today, credit: amount, debit: '', balance: balance)
+    @all_transactions.unshift(date: today, credit: twodecimal(amount), debit: '', balance: twodecimal(balance))
   end
 
   def save_withdrawal(amount, balance)
-    @all_transactions.unshift(date: today, credit: '', debit: amount, balance: balance)
+    @all_transactions.unshift(date: today, credit: '', debit: twodecimal(amount), balance: twodecimal(balance))
   end
 
   def today
     Date.today.strftime('%d/%m/%Y')
   end
+
+  def twodecimal(amount)
+    format('%.2f', amount)
+  end
+
 end
--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe Display do
     it 'should print out the history of my account: one deposit' do
       account_history = double('account_history')
       display = Display.new(account_history)
-      allow(account_history).to receive(:all_transactions).and_return([{date: '04/02/2019', credit: display.twodecimal(2000), debit: '', balance: display.twodecimal(2000)}])
+      allow(account_history).to receive(:all_transactions).and_return([{date: '04/02/2019', credit: '2000.00', debit: '', balance: '2000.00'}])
       expect { display.print_statement }.to output("date || credit || debit || balance\n04/02/2019 || 2000.00 ||  || 2000.00\n").to_stdout
     end
 
     it 'should print out the history of my account: multiple transactions' do
       account_history = double('account_history')
       display = Display.new(account_history)
-      allow(account_history).to receive(:all_transactions).and_return([{date: '04/02/2019', credit: '', debit: display.twodecimal(500), balance: display.twodecimal(1500)}, {date: '04/02/2019', credit: display.twodecimal(2000), debit: '', balance: display.twodecimal(2000)}])
+      allow(account_history).to receive(:all_transactions).and_return([{date: '04/02/2019', credit: '', debit: '500.00', balance: '1500.00'}, {date: '04/02/2019', credit: '2000.00', debit: '', balance: '2000.00'}])
       expect { display.print_statement }.to output("date || credit || debit || balance\n04/02/2019 ||  || 500.00 || 1500.00\n04/02/2019 || 2000.00 ||  || 2000.00\n").to_stdout
     end
   end
