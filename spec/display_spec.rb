4058fd1f69ca88178756e3ef4f6e7dfa4719db4a
debora <debora.piu92@gmail.com>
2019-03-12 16:34:48 +0000

    tests for display are modified due to transaction objects

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -3,18 +3,11 @@ require 'display'
 RSpec.describe Display do
 
   context "#print_statement" do
-    it 'should print out the history of my account: one deposit' do
+    it 'should print out the history of my account: no deposits' do
       account_history = double('account_history')
       display = Display.new(account_history)
-      allow(account_history).to receive(:all_transactions).and_return([{date: '04/02/2019', credit: 2000, debit: 0, balance: 2000}])
-      expect { display.print_statement }.to output("date || credit || debit || balance\n04/02/2019 || 2000.00 ||  || 2000.00\n").to_stdout
-    end
-
-    it 'should print out the history of my account: multiple transactions' do
-      account_history = double('account_history')
-      display = Display.new(account_history)
-      allow(account_history).to receive(:all_transactions).and_return([{date: '04/02/2019', credit: 0, debit: 500, balance: 1500}, {date: '04/02/2019', credit: 2000, debit: 0, balance: 2000}])
-      expect { display.print_statement }.to output("date || credit || debit || balance\n04/02/2019 ||  || 500.00 || 1500.00\n04/02/2019 || 2000.00 ||  || 2000.00\n").to_stdout
+      allow(account_history).to receive(:all_transactions).and_return([])
+      expect { display.print_statement }.to output("date || credit || debit || balance\n").to_stdout
     end
   end
 end
