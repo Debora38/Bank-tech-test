fe141a9b90786df99dbe45ff76f204b950c2ccd6
debora <debora.piu92@gmail.com>
2019-03-12 18:15:49 +0000

    save transactions methods - commented extra line removed

--- lib/account_history.rb
+++ lib/account_history.rb
@@ -11,12 +11,10 @@ class AccountHistory
 
   def save_deposit(amount, balance)
     @all_transactions.unshift(@transaction.new(today, amount, 0, balance))
-    # (date: today, credit: amount, debit: 0, balance: balance)
   end
 
   def save_withdrawal(amount, balance)
     @all_transactions.unshift(@transaction.new(today, 0, amount, balance))
-    # (date: today, credit: 0, debit: amount, balance: balance)
   end
 
   def today
